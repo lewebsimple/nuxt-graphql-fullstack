4f0439ece75bef7419815e5b0b4bc347f4161bfb
Pascal Martineau <pascal@lewebsimple.ca>
2023-07-15 21:02:19 -0400

    refactor: Prefix models with Auth

--- server/prisma/migrations/20230716010126_auth/migration.sql
+++ server/prisma/migrations/20230716010126_auth/migration.sql
@@ -1,39 +1,39 @@
 -- CreateTable
-CREATE TABLE `User` (
+CREATE TABLE `AuthUser` (
     `id` VARCHAR(191) NOT NULL,
     `email` VARCHAR(191) NOT NULL,
 
-    UNIQUE INDEX `User_id_key`(`id`),
-    UNIQUE INDEX `User_email_key`(`email`),
+    UNIQUE INDEX `AuthUser_id_key`(`id`),
+    UNIQUE INDEX `AuthUser_email_key`(`email`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `Session` (
+CREATE TABLE `AuthSession` (
     `id` VARCHAR(191) NOT NULL,
     `user_id` VARCHAR(191) NOT NULL,
     `active_expires` BIGINT NOT NULL,
     `idle_expires` BIGINT NOT NULL,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
-    UNIQUE INDEX `Session_id_key`(`id`),
-    INDEX `Session_user_id_idx`(`user_id`),
+    UNIQUE INDEX `AuthSession_id_key`(`id`),
+    INDEX `AuthSession_user_id_idx`(`user_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `Key` (
+CREATE TABLE `AuthKey` (
     `id` VARCHAR(191) NOT NULL,
     `hashed_password` VARCHAR(191) NULL,
     `user_id` VARCHAR(191) NOT NULL,
 
-    UNIQUE INDEX `Key_id_key`(`id`),
-    INDEX `Key_user_id_idx`(`user_id`),
+    UNIQUE INDEX `AuthKey_id_key`(`id`),
+    INDEX `AuthKey_user_id_idx`(`user_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `Session` ADD CONSTRAINT `Session_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `AuthSession` ADD CONSTRAINT `AuthSession_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `AuthUser`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `Key` ADD CONSTRAINT `Key_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `AuthKey` ADD CONSTRAINT `AuthKey_user_id_fkey` FOREIGN KEY (`user_id`) REFERENCES `AuthUser`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
--- server/utils/auth.ts
+++ server/utils/auth.ts
@@ -3,7 +3,14 @@ import { lucia } from "lucia";
 import { h3 } from "lucia/middleware";
 
 export const auth = lucia({
-  adapter: prismaAdapter(prisma),
+  adapter: prismaAdapter(prisma, {
+    modelNames: {
+      user: "authUser",
+      key: "authKey",
+      session: "authSession",
+    },
+    userRelationKey: "user",
+  }),
   env: process.dev ? "DEV" : "PROD",
   middleware: h3(),
   getUserAttributes: (data) => ({
