e05feff5abd4aab349fed97541b676d24bac538d
Pascal Martineau <pascal@lewebsimple.ca>
2023-07-27 14:56:22 -0400

    fix: authScopes on AuthUser

--- server/graphql/types/auth-user.ts
+++ server/graphql/types/auth-user.ts
@@ -7,6 +7,7 @@ export const user = () => {
     fields: (t) => ({
       email: t.exposeString("email"),
     }),
+    authScopes: ({ id }, { session }) => session?.user.userId === id,
   });
 
   // authUsers query
@@ -17,6 +18,7 @@ export const user = () => {
       defaultSize: 2,
       totalCount: () => prisma.authUser.count(),
       resolve: (query, _root, _args, { prisma }) => prisma.authUser.findMany({ ...query }),
+      authScopes: { isAuthenticated: true },
     }),
   );
 };
--- server/prisma/migrations/20230726174017_auth/migration.sql
+++ server/prisma/migrations/20230726174017_auth/migration.sql
@@ -3,7 +3,6 @@ CREATE TABLE `AuthUser` (
     `id` VARCHAR(191) NOT NULL,
     `email` VARCHAR(191) NOT NULL,
 
-    UNIQUE INDEX `AuthUser_id_key`(`id`),
     UNIQUE INDEX `AuthUser_email_key`(`email`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -16,7 +15,6 @@ CREATE TABLE `AuthSession` (
     `idle_expires` BIGINT NOT NULL,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
-    UNIQUE INDEX `AuthSession_id_key`(`id`),
     INDEX `AuthSession_user_id_idx`(`user_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -27,7 +25,6 @@ CREATE TABLE `AuthKey` (
     `hashed_password` VARCHAR(191) NULL,
     `user_id` VARCHAR(191) NOT NULL,
 
-    UNIQUE INDEX `AuthKey_id_key`(`id`),
     INDEX `AuthKey_user_id_idx`(`user_id`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
