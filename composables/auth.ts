9e33df4f763a2bef300748c011721cc737b63666
Pascal Martineau <pascal@lewebsimple.ca>
2023-08-03 08:15:47 -0400

    chore: update deps

--- composables/auth.ts
+++ composables/auth.ts
@@ -9,25 +9,27 @@ export function useAuth() {
     session.value?.user && [AuthRole.ADMINISTRATOR, role].includes(session.value.user.role);
 
   async function login(body: { email: string; password: string }) {
-    const { session: newSession, error } = (await $fetch("/api/login", { method: "POST", body })) as {
-      session: Session | null;
-      error: string | null;
-    };
+    const { session: newSession, error } = await $fetch<{ session: Session | null; error: string | null }>(
+      "/api/login",
+      { method: "POST", body },
+    );
     session.value = newSession;
     if (error) throw new Error(error);
   }
 
   async function logout() {
-    const { session: newSession, error } = await $fetch("/api/logout", { method: "POST" });
+    const { session: newSession, error } = await $fetch<{ session: null; error: string | null }>("/api/logout", {
+      method: "POST",
+    });
     session.value = newSession;
     if (error) throw new Error(error);
   }
 
   async function signup(body: { email: string; password: string }) {
-    const { session: newSession, error } = (await $fetch("/api/signup", { method: "POST", body })) as {
-      session: Session | null;
-      error: string | null;
-    };
+    const { session: newSession, error } = await $fetch<{ session: Session | null; error: string | null }>(
+      "/api/signup",
+      { method: "POST", body },
+    );
     session.value = newSession;
     if (error) throw new Error(error);
   }
