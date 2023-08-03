5db2538f0a9d03eb5d6439b8f1abc870a73473f2
Pascal Martineau <pascal@lewebsimple.ca>
2023-08-02 22:20:02 -0400

    refactor: Healthcheck worker validation with Zod

--- server/workers/healthcheck.ts
+++ server/workers/healthcheck.ts
@@ -1,14 +1,21 @@
 import { type Job, Worker } from "bullmq";
+import { z } from "zod";
 
 const { connection } = useRuntimeConfig().bullmq;
 
-export type HealthcheckDataType = { message: string };
-export type HealthcheckReturnType = string;
+const HealthCheckDataTypeSchema = z.object({
+  message: z.string(),
+});
+export type HealthcheckDataType = z.input<typeof HealthCheckDataTypeSchema>;
+
+const HealthcheckReturnTypeSchema = z.string();
+export type HealthcheckReturnType = z.input<typeof HealthcheckReturnTypeSchema>;
 
 export const healthcheck = new Worker<HealthcheckDataType, HealthcheckReturnType>(
   "healthcheck",
   async (job: Job) => {
-    console.log(`Healthcheck worker received message "${job.data.message}" from job ID ${job.id}`);
+    const data = HealthCheckDataTypeSchema.parse(job.data);
+    console.log(`Healthcheck worker received message "${data.message}" from job ID ${job.id}`);
     await new Promise((resolve) => setTimeout(resolve, 1000));
     return `Healthcheck worker completed job ID ${job.id}`;
   },
