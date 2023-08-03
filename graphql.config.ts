c8dbbff6c93ecd555f6127d1ce4760bf1027b2e1
Pascal Martineau <pascal@lewebsimple.ca>
2023-08-03 08:18:12 -0400

    feat: graphql config in default project

--- graphql.config.ts
+++ graphql.config.ts
@@ -4,14 +4,18 @@ import type { IGraphQLConfig } from "graphql-config";
 import { schema } from "./server/graphql/schema";
 
 const config: IGraphQLConfig = {
-  schema: printSchema(schema),
-  documents: ["./components/**/*.vue", "composables/**/*.ts"],
-  extensions: {
-    codegen: {
-      generates: {
-        "./composables/generated/": {
-          preset: "client",
-          config: { avoidOptionals: true, useTypeImports: true },
+  projects: {
+    default: {
+      schema: printSchema(schema),
+      documents: ["./components/**/*.vue", "composables/**/*.ts"],
+      extensions: {
+        codegen: {
+          generates: {
+            "./composables/generated/": {
+              preset: "client",
+              config: { avoidOptionals: true, useTypeImports: true },
+            },
+          },
         },
       },
     },
