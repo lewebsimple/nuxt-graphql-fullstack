f81c827372dd9f2d9cdf4dfaff742c3f05bd26ec
Pascal Martineau <pascal@lewebsimple.ca>
2023-11-15 11:30:04 -0500

    feat: Save GraphQL schema to file from code

--- server/graphql/schema.ts
+++ server/graphql/schema.ts
@@ -1,3 +1,6 @@
+import { writeFileSync } from "fs";
+import { lexicographicSortSchema, printSchema } from "graphql";
+
 import { builder } from "./builder";
 import * as types from "./types";
 
@@ -8,3 +11,9 @@ builder.queryType({});
 Function.prototype(types);
 
 export const schema = builder.toSchema();
+
+// Save GraphQL schema to file
+if (process.env.NODE_ENV !== "production") {
+  const schemaAsString = printSchema(lexicographicSortSchema(schema));
+  writeFileSync("generated/graphql/schema.graphql", schemaAsString);
+}
