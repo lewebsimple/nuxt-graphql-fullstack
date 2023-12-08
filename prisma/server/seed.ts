e1dcfad32848a362a5c21709c6b6bb3e886a1d21
Pascal Martineau <pascal@lewebsimple.ca>
2023-12-08 11:30:13 -0500

    minor: explicit seeds object

--- prisma/server/seed.ts
+++ prisma/server/seed.ts
@@ -3,8 +3,12 @@ import { prisma, type PrismaClient } from "~/prisma/server/utils/prisma";
 
 export type SeedFn = (prisma: PrismaClient) => Promise<any>;
 
+const seeds: Record<string, SeedFn> = {
+  seedAdminUser,
+};
+
 async function main() {
-  for (const [seedName, seedFn] of Object.entries({ seedAdminUser })) {
+  for (const [seedName, seedFn] of Object.entries(seeds)) {
     console.log("\n🌱 " + seedName);
     console.log(await seedFn(prisma));
   }
