463fce0f1fbdcf88eeb270b4fd23e517fb827456
Pascal Martineau <pascal@lewebsimple.ca>
2023-12-08 11:25:41 -0500

    minor: disable ui components path prefix

--- ui/nuxt.config.ts
+++ ui/nuxt.config.ts
@@ -1,4 +1,5 @@
 export default defineNuxtConfig({
+  components: [{ path: "~/ui/components", pathPrefix: false }],
   modules: ["@nuxt/ui"],
   tailwindcss: {
     cssPath: "~/ui/assets/css/main.css",
